839b85b71c4b19b1f22af67facfcd6f091a96670
slmtpz <seleme94@hotmail.com>
2018-10-13 14:43:11 +0300

    server settings are adjusted so that frontend developer can access the server on backend developers laptop

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+
 @app.route('/')
-def hello_world():
-  return render_template('index.html')
+def main_page():
+    return render_template('index.html')
+
 
 if __name__ == '__main__':
-  app.run(debug=True)
+    app.run('0.0.0.0', 5000, debug=True)
 

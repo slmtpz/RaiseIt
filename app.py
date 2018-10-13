0bb057a724c983b3c4e9b6e86dba6713789032eb
slmtpz <seleme94@hotmail.com>
2018-10-13 16:43:33 +0300

    mongo authentication fixes

--- app.py
+++ app.py
@@ -3,9 +3,9 @@ from flask_pymongo import PyMongo
 import config
 
 app = Flask(__name__)
-app.config["MONGO_URI"] = "mongodb://%s:%s@%s/%s" % (config.MONGO['USER_NAME'],
+app.config["MONGO_URI"] = "mongodb://%s:%s@%s/%s" % (config.MONGO['USERNAME'],
                                                      config.MONGO['PASSWORD'],
-                                                     config.MONGO['URL'],
+                                                     config.MONGO['HOSTPORT'],
                                                      config.MONGO['DATABASE'])
 mongo = PyMongo(app)
 
@@ -16,5 +16,5 @@ def main_page():
 
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', 5000, debug=True)
+    app.run('0.0.0.0', 5000)
 

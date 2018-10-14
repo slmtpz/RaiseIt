3c9950a04bdf7a972489868367b746700b4b8dce
Orbay Altuntoprak <orbay.altuntoprak@bitaksi.com>
2018-10-14 12:53:04 +0300

    add gsm to sign up form

--- js/components/Home.js
+++ js/components/Home.js
@@ -1,23 +1,45 @@
 import React, {Component} from 'react';
 
 import AdMap from './AdMap';
-import PostList from './PostList';
+import TableWrap from './TableWrap';
+import requestHandler from './../RequestHandler';
 import './Home.css'
 
 class Home extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			postings: [],
+		}
+	}
+
+	componentDidMount() {
+		requestHandler.get('/posting').then(e => {
+			// console.log(e.data.postings);
+			this.setState({postings: e.data.postings});
+		});
+		// console.log(localStorage.getItem('username'));
+		// localStorage.setItem('username', 'naber');
+		// localStorage.clear();
+	}
+
+
 	render(){
-		console.log(this.props);
 		return (
-			<div className='home-grid'>
+			<div style={{margin: "0px 15% 0px"}}>
 				<AdMap
 					events={this.props.events}
 					googleMapURL={`https://maps.googleapis.com/maps/api/js?key=AIzaSyAdvV5oYS9vTziqOCBJ1oag-ABFsuenhBQ&v=3.exp&libraries=geometry,drawing,places`}
-					loadingElement={<div style={{ height: `40%` }} />}
-					containerElement={<div/>}
-					mapElement={<div style={{ height: `40%` }} />}
+					loadingElement={<div style={{ height: `100%` }} />}
+					containerElement={<div style={{height: `400px`}}/>}
+					mapElement={<div style={{ height: `100%` }} />}
 			    />
-				{/* <MeinList /> */}
-				<PostList />
+				{this.state.postings.length > 0 &&
+				<TableWrap
+					data={this.state.postings}
+				/>}
+				{/* TO DO: ADD LOADER. */}
 			</div>
 		);
 	}
